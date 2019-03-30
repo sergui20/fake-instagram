0c735ba2a977133a42ed6d75cb8c0503c8cd3bc8
Sergui <serguinajibm20@gmail.com>
2019-03-30 18:04:43 -0500

    Fix deploy

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const util = require('util');
 
 // Models
 const User = require('../models/users');
--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ require('./config/config');
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
-const morgan = require('morgan');
 
 const app = express();
 
@@ -20,7 +19,6 @@ app.set('port', process.env.PORT || 8080);
 app.locals.isAuthenticated = false;
 
 // Middlewares
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
 app.use(cookieParser())
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
